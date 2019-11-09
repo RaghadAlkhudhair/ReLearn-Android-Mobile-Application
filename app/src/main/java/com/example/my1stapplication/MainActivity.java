7ce79455655f49b538f758b7b36456a53baf8b6d
raghadalkhudhair <JAN2019Sec>
2019-11-09 04:25:44 +0300

    message

--- app/src/main/java/com/example/my1stapplication/MainActivity.java
+++ app/src/main/java/com/example/my1stapplication/MainActivity.java
@@ -130,7 +130,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         // the auth state listener will be notified and logic to handle the
                         // signed in user can be handled in the listener.
                         if (!task.isSuccessful()) {
-                            Toast.makeText(MainActivity.this, "Authentication failed." + task.getException(),
+                            Toast.makeText(MainActivity.this, "Authentication failed,please check email and password are right." + task.getException(),
                                     Toast.LENGTH_SHORT).show();
                             mDialog.dismiss();
                         } else {
